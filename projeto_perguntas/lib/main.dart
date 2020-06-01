4e97bfd40949dfba38875984a06b71efdf955e82
yure <yurematias@ufrn.edu.br>
2020-05-31 23:02:57 -0300

    botoes adicionados no body de Scaffold

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -5,17 +5,49 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-
   // quem passa o context é o próprio Flutter
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = <String>[
+      'Qual seu anime favorito?',
+      'Qual sua banda favorita?',
+      'Qual seu game favorito?'
+    ];
+
+    void responder() {
+      print('Pergunta respondida');
+    }
+
     return MaterialApp(
-      // Scaffold é como uma estrutura 
+      // Scaffold é como uma estrutura
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter'),
+        // o body pode receber mais de um Widget
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[2]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: () {
+                print('Resposta 2');
+              },
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: () => print('Resposta 3'),
+            ),
+            RaisedButton(
+              child: Text('Botão desabilitado'),
+              onPressed: null,
+            ),
+          ],
+        ),
       )
     );
   }

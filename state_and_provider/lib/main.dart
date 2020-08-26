fe48c2eee593dda5e6cc890430a42023147c5fc0
Yure Matias <yurematias@ufrn.edu.br>
2020-08-26 15:01:40 -0300

    conteudo sobre estado e provider

---
+++ state_and_provider/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:state_and_provider/providers/CounterProvider.dart';
+
+main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CounterProvider(
+      child: MaterialApp(
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<StatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Working with State and Provider'),
+      ),
+      body: Column(
+        children: [
+          Text('${CounterProvider.of(context).state.value}'),
+          RaisedButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                CounterProvider.of(context).state.increment();
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

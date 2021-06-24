741caeca9d018fc8eb3ca63cd7386942b8ba0af6
yuredev <yurematias@ufrn.edu.br>
2021-06-24 15:32:30 -0300

    Classe para CRUD de notas finalizada

--- sqflite_sqlite/lib/database/notes_database.dart
+++ sqflite_sqlite/lib/database/notes_database.dart
@@ -20,7 +20,6 @@ class NotesDatabase {
   }
 
   Future<Database> _initDB(String fileName) async {
-
     // getDatabasesPath() retorna o caminho para o banco, no caso do Android é data/data/databases algo assim
     final dbPath = await getDatabasesPath();
     final completePath = path.join(dbPath, fileName);
@@ -36,9 +35,83 @@ class NotesDatabase {
         ${NoteTableColumns.number} ${SQLTypes.integer},
         ${NoteTableColumns.title} ${SQLTypes.text},
         ${NoteTableColumns.description} ${SQLTypes.text},
-        ${NoteTableColumns.createdTime} ${SQLTypes.text},
+        ${NoteTableColumns.time} ${SQLTypes.text},
       );
     ''');
+  }
+
+  Future<Note> create(Note note) async {
+    final db = await instance.database;
+    // para inserir é preciso converter para json antes
+    // o sqflite retorna o id do novo registro no banco
+    // caso o id não seja passado ele gera um id novo e o retorna
+    // caso seja passado ele retorna o id passado dentro do objeto a ser criado
+    int id = await db.insert(noteTableName, note.toJSON());
+
+    // rodar sql bruto no banco
+    // await db.rawInsert('INSERT INTO...');
+
+    return note.copy(id: id);
+  }
+
+  Future<Note?> read(int id) async {
+    final db = await instance.database;
+
+    final jsonNotes = await db.query(
+      noteTableName,
+      where: '${NoteTableColumns.id} = ?',
+      columns: NoteTableColumns.values,
+      // o valor colocado no whereArgs substitui o ? do where
+      // obs: não é recomendado colocar direto no where pois
+      // deixa vuneravel a SQL Injection
+      whereArgs: [id],
+
+      // usando mais de um whereArgs
+      // where: '${NoteTableColumns.id} = ? AND ${NoteTableColumns.title} = ?',
+      // whereArgs: [id, title],
+    );
+
+    if (jsonNotes.isNotEmpty) {
+      return Note.fromJSON(jsonNotes.first);
+    }
+
+    return null;
+  }
+
+  Future<List<Note>> readAll() async {
+    final db = await instance.database;
+    final result = await db.query(
+      noteTableName,
+      columns: NoteTableColumns.values,
+      orderBy: '${NoteTableColumns.time} ASC',
+    );
+
+    return result.map((jsonNote) => Note.fromJSON(jsonNote)).toList();
+  }
+
+  Future<int> update(Note note) async {
+    final db = await instance.database;
+
+    final result = await db.update(
+      noteTableName,
+      note.toJSON(),
+      where: '${NoteTableColumns.id} = ?',
+      whereArgs: [note.id],
+    );
+
+    return result;
+  }
+
+  Future<int> delete(Note note) async {
+    final db = await instance.database;
+
+    final result = await db.delete(
+      noteTableName,
+      where: '${NoteTableColumns.id} = ?',
+      whereArgs: [note.id],
+    );
+
+    return result;
   }
 
   Future close() async {
--- sqflite_sqlite/lib/models/note.dart
+++ sqflite_sqlite/lib/models/note.dart
@@ -6,7 +6,16 @@ class NoteTableColumns {
   static final number = 'number';
   static final title = 'title';
   static final description = 'description';
-  static final createdTime = 'createdTime';
+  static final time = 'createdTime';
+
+  static final List<String> values = [
+    id,
+    isImportant,
+    number,
+    title,
+    description,
+    time
+  ];
 }
 
 class Note {
@@ -25,4 +34,42 @@ class Note {
     required this.description,
     required this.createdTime,
   });
+
+  Map<String, Object?> toJSON() => {
+        NoteTableColumns.id: id,
+        NoteTableColumns.title: title,
+        NoteTableColumns.description: description,
+        // valores boleanos no SQL são 0 ou 1
+        NoteTableColumns.isImportant: isImportant ? 1 : 0,
+        NoteTableColumns.number: number,
+        NoteTableColumns.time: createdTime.toIso8601String(),
+      };
+
+  static Note fromJSON(Map<String, Object?> jsonNote) {
+    return Note(
+      id: jsonNote[NoteTableColumns.id] as int?,
+      title: jsonNote[NoteTableColumns.title] as String,
+      createdTime: DateTime.parse([NoteTableColumns.time] as String),
+      description: jsonNote[NoteTableColumns.description] as String,
+      isImportant: (jsonNote[NoteTableColumns.isImportant] as int) == 0 ? false : true,
+      number: jsonNote[NoteTableColumns.number] as int,
+    );
+  }
+
+  Note copy({
+    int? id,
+    bool? isImportant = false,
+    int? number,
+    String? title,
+    String? description,
+    DateTime? createdTime,
+  }) =>
+      Note(
+        id: id ?? this.id,
+        isImportant: isImportant ?? this.isImportant,
+        number: number ?? this.number,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        createdTime: createdTime ?? this.createdTime,
+      );
 }
